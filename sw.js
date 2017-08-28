695c22fd5097185b92f587e0f158e9cb9b776cea
trystian1 <trystiano@hotmail.com>
2017-08-28 23:03:53 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,58 @@
+var currentCacheName = 'cache-v3';
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(currentCacheName).then(function(cache) {
+      return cache.addAll([
+        '/',
+        'css/styles.css',
+        'css/responsive-grid.css',
+        'data/restaurants.json',
+        'js/main.js',
+        'js/restaurant_info.js',
+        'js/dbhelper.js',
+        'restaurant.html'
+      ])
+    })
+  )
+});
+
+self.addEventListener('fetch', function(event) {
+
+  var requestUrl = new URL(event.request.url);
+
+  if (requestUrl.origin === location.origin) {
+    if (requestUrl.pathname === '/') {
+
+      if (caches.match('/')) {
+        event.respondWith(caches.match('/'));
+      }
+
+      return;
+
+    }
+
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return cacheName.startsWith('cache-') &&
+                 cacheName !== currentCacheName
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
