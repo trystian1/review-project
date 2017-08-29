eba90db4a8ab8b3606d82feadb853c31dba1aa50
trystian1 <trystian.offerman@gmail.com>
2017-08-29 21:45:00 +0200

    Review-comments: fixed review comments, fixed aria roles, fixed big map, fixed tabindex

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -20,6 +20,7 @@ window.initMap = () => {
   });
 }
 
+
 /**
  * Get current restaurant from page URL.
  */
@@ -51,9 +52,11 @@ fetchRestaurantFromURL = (callback) => {
 fillRestaurantHTML = (restaurant = self.restaurant) => {
   const name = document.getElementById('restaurant-name');
   name.innerHTML = restaurant.name;
+  name.tabIndex = "0";
 
   const address = document.getElementById('restaurant-address');
   address.innerHTML = restaurant.address;
+  address.tabIndex = "0";
 
   const image = document.getElementById('restaurant-img');
   image.className = 'restaurant-img'
@@ -61,7 +64,8 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
   image.alt = 'photo of ' + restaurant.name;
 
   const cuisine = document.getElementById('restaurant-cuisine');
-  cuisine.innerHTML = restaurant.cuisine_type;
+  cuisine.innerHTML = restaurant.cuisine_type
+  cuisine.tabIndex = "0";
 
   // fill operating hours
   if (restaurant.operating_hours) {
@@ -81,10 +85,13 @@ fillRestaurantHoursHTML = (operatingHours = self.restaurant.operating_hours) => 
 
     const day = document.createElement('td');
     day.innerHTML = key;
+    day.tabIndex = "0";
     row.appendChild(day);
 
     const time = document.createElement('td');
     time.innerHTML = operatingHours[key];
+    time.tabIndex = "0";
+
     row.appendChild(time);
 
     hours.appendChild(row);
@@ -96,7 +103,7 @@ fillRestaurantHoursHTML = (operatingHours = self.restaurant.operating_hours) => 
  */
 fillReviewsHTML = (reviews = self.restaurant.reviews) => {
   const container = document.getElementById('reviews-container');
-  container.className = 'small-12 medium-6 large-6';
+  container.className = 'small-12 medium-5 large-5';
   const title = document.createElement('h2');
   title.innerHTML = 'Reviews';
   container.appendChild(title);
@@ -121,18 +128,22 @@ createReviewHTML = (review) => {
   const li = document.createElement('li');
   const name = document.createElement('p');
   name.innerHTML = review.name;
+  name.tabIndex = "0";
   li.appendChild(name);
 
   const date = document.createElement('p');
-  date.innerHTML = review.date;
+  date.innerHTML = review.date
+  date.tabIndex = "0";
   li.appendChild(date);
 
   const rating = document.createElement('p');
   rating.innerHTML = `Rating: ${review.rating}`;
+  rating.tabIndex = "0";
   li.appendChild(rating);
 
   const comments = document.createElement('p');
   comments.innerHTML = review.comments;
+  comments.tabIndex = "0";
   li.appendChild(comments);
 
   return li;
